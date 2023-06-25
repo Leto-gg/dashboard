93d22c264d6e652748445ec1082417514755eb7b
Yasir Nadeem <m-yasir.dev@protonmail.com>
2023-06-25 15:04:52 +0500

    feat: implement not found page

---
+++ src/pages/notFound/index.jsx
@@ -0,0 +1,24 @@
+import { Button, Typography } from "@mui/material";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+`;
+
+export function NotFound() {
+  return (
+    <Container>
+      <Typography variant="overline" aria-label="">
+        Oops! <br />
+        You seem to have stumbled upon a page that does not exist
+      </Typography>
+      <Typography variant="caption">Wanna fix? Press this button</Typography>
+      <Link to="/">
+        <Button variant="contained">Home</Button>
+      </Link>
+    </Container>
+  );
+}
