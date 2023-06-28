3e8352fc7ee61ffdb16815bffbca3169304843d3
Yasir Nadeem <m-yasir.dev@protonmail.com>
2023-06-28 22:08:18 +0500

    feat: define routes for the app according to react router v6

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,9 +1,26 @@
+import { Dashboard } from "../pages/dashboard";
+import { Settings } from "../pages/settings";
 import { NotFound } from "../pages/notFound";
+
 import { Root } from "./root";
 
 /**
  * @type {import("react-router-dom").RouteObject[]}
  */
 export const routes = [
-  { path: "/", element: <Root />, errorElement: <NotFound /> },
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <NotFound />,
+    children: [
+      {
+        path: "/",
+        element: <Dashboard />,
+      },
+      {
+        path: "/settings",
+        element: <Settings />,
+      },
+    ],
+  },
 ];
