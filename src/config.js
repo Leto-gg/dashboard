b6c10c3ddb721f01edab0dfba49c9f788fde6b66
Yasir Nadeem <m-yasir.dev@protonmail.com>
2023-06-28 22:05:40 +0500

    feat: add app config

---
+++ src/config.js
@@ -0,0 +1,11 @@
+export const config = {
+  defaultPath: "/",
+  fontFamily: `'Public Sans', sans-serif`,
+  i18n: "en",
+  miniDrawer: false,
+  container: true,
+  mode: "light",
+  presetColor: "default",
+  themeDirection: "ltr",
+  drawerWidth: 260,
+};
