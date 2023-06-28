c0346dabe9aa5aae63387482757917d29f6bad8f
Yasir Nadeem <m-yasir.dev@protonmail.com>
2023-06-28 22:08:34 +0500

    feat: update root layout to include sidenav

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,5 +1,60 @@
+import { MainDrawer } from "../components/organisms/mainDrawer";
+import DrawerHeader from "../components/molecules/drawerHeader";
+import { useEffect, useState } from "react";
+import { Outlet } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+
+// material-ui
+import { useTheme } from "@mui/material/styles";
+import { Box, Toolbar, useMediaQuery } from "@mui/material";
+
+// project import
+// import navigation from "menu-items";
+// import Breadcrumbs from "components/@extended/Breadcrumbs";
 import "./root.css";
 
+// types
+import { openDrawer } from "../libs/redux/slices/drawer.slice";
+
 export function Root() {
-  return <h3>Root Layout</h3>;
+  const theme = useTheme();
+  const matchDownLG = useMediaQuery(theme.breakpoints.down("lg"));
+  const dispatch = useDispatch();
+
+  const { drawerOpen } = useSelector((state) => state.drawer);
+
+  // drawer toggler
+  const [open, setOpen] = useState(drawerOpen);
+  const handleDrawerToggle = () => {
+    setOpen(!open);
+    dispatch(openDrawer({ drawerOpen: !open }));
+  };
+
+  // set media wise responsive drawer
+  useEffect(() => {
+    setOpen(!matchDownLG);
+    dispatch(openDrawer({ drawerOpen: !matchDownLG }));
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [matchDownLG]);
+
+  useEffect(() => {
+    if (open !== drawerOpen) setOpen(drawerOpen);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [drawerOpen]);
+
+  return (
+    <Box sx={{ display: "flex", width: "100%" }}>
+      <DrawerHeader open={open} handleDrawerToggle={handleDrawerToggle} />
+      <MainDrawer open={open} handleDrawerToggle={handleDrawerToggle} />
+      <Box
+        component="main"
+        sx={{ width: "100%", flexGrow: 1, p: { xs: 2, sm: 3 } }}
+      >
+        <Toolbar />
+        {/* <Breadcrumbs navigation={navigation} title /> */}
+        <Outlet />
+      </Box>
+    </Box>
+  );
 }
