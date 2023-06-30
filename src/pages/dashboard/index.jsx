4d88b95b194742f23e2e3e22fa431e437139333a
Yasir Nadeem <m-yasir.dev@protonmail.com>
2023-06-30 23:56:08 +0500

    feat(dashboard): integrate bar chart and analytics api fetch

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -1,41 +1,60 @@
-import { Box, Button, Grid, Stack, Typography } from "@mui/material";
+import { useMemo } from "react";
+
+import { Alert, Box, Grid, Typography } from "@mui/material";
 
 import { MainCard } from "./../../components/molecules/mainCard";
-import { useState } from "react";
-import { LineChart } from "../../components/molecules/lineChart";
+import { BarChart } from "../../components/molecules/barChart";
+import { middleTruncate } from "../../libs/utils/string.helpers";
+import { FETCHING_STATES, useFetch } from "../../hooks/useFetch";
+import { getAnalytics } from "../../api/analytics.api";
+import { useEffect } from "react";
 
 function CIDAnalytics() {
-  const [slot, setSlot] = useState("week");
+  const [{ data, type }, handleFetch] = useFetch();
+
+  const labels = useMemo(
+    () => data?.data?.map((content) => middleTruncate(content.cid, 6)) ?? [],
+    [data]
+  );
+  const dataPoints = useMemo(
+    () => data?.data?.map((data) => data.numbersAccessed) ?? [],
+    [data]
+  );
+
+  useEffect(() => {
+    handleFetch(() =>
+      getAnalytics(
+        // pass in CIDs that user saved, to get their respective analytics
+        []
+      )
+    );
+  }, [handleFetch]);
+
   return (
-    <Grid item xs={12} md={7} lg={8}>
-      <Grid container alignItems="center" justifyContent="space-between">
-        <Grid item>
-          <Typography variant="h5">CID Analytics</Typography>
-        </Grid>
-        <Grid item>
-          <Stack direction="row" alignItems="center" spacing={0}>
-            <Button
-              size="small"
-              onClick={() => setSlot("month")}
-              color={slot === "month" ? "primary" : "secondary"}
-              variant={slot === "month" ? "outlined" : "text"}
-            >
-              Year
-            </Button>
-            <Button
-              size="small"
-              onClick={() => setSlot("week")}
-              color={slot === "week" ? "primary" : "secondary"}
-              variant={slot === "week" ? "outlined" : "text"}
-            >
-              Week
-            </Button>
-          </Stack>
-        </Grid>
+    <Grid item>
+      <Grid item>
+        <Typography variant="h5">CID Analytics</Typography>
       </Grid>
       <MainCard content={false} sx={{ mt: 1.5 }}>
-        <Box sx={{ pt: 1, pr: 2 }}>
-          <LineChart slot={slot} />
+        <Box sx={{ padding: 2 }}>
+          {type === FETCHING_STATES.DONE && (
+            <BarChart
+              xAxisLabel="numbersAccessed"
+              labels={labels}
+              dataPoints={dataPoints}
+            />
+          )}
+          {type === FETCHING_STATES.PENDING && (
+            <Typography padding={4} variant="body1">
+              Loading...
+            </Typography>
+          )}
+          {type === FETCHING_STATES.FAIL && (
+            <Alert severity="error">
+              Failed to fetch data for the chart, we are working on fixing it as
+              soon as possible!
+            </Alert>
+          )}
         </Box>
       </MainCard>
     </Grid>
@@ -44,9 +63,9 @@ function CIDAnalytics() {
 
 export default function Dashboard() {
   return (
-    <>
+    <Grid>
       <h3>Welcome, John Doe!</h3>
       <CIDAnalytics />
-    </>
+    </Grid>
   );
 }
