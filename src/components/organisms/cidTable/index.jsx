0affa4be7425a4634b746f2e75f76c66d559e269
Yasir Nadeem <m-yasir.dev@protonmail.com>
2023-07-02 05:21:54 +0500

    refactor(pages/cids): move CIDTable to its own component file

---
+++ src/components/organisms/cidTable/index.jsx
@@ -0,0 +1,86 @@
+import PropTypes from "prop-types";
+
+import {
+  Chip,
+  CircularProgress,
+  IconButton,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material";
+import Typography from "@mui/material/Typography";
+
+import { getFormattedDate } from "../../../libs/utils/date.helpers";
+import { DeleteOutlined } from "@ant-design/icons";
+
+function getCidTypeChipColor(cidType) {
+  switch (cidType) {
+    case "ipfs":
+      return "primary";
+    case "ipns":
+      return "info";
+    default:
+      return "primary";
+  }
+}
+
+/**
+ * @param {{ cids: any[], isLoading: boolean, onRemove: function }} props
+ * @returns
+ */
+export function CIDTable({ cidAnalytics = [], isLoading = false, onRemove }) {
+  if (isLoading) {
+    return <CircularProgress variant="indeterminate" />;
+  }
+
+  if (cidAnalytics.length === 0) {
+    return (
+      <Typography variant="body1">No CIDs are being tracked by you</Typography>
+    );
+  }
+
+  return (
+    <TableContainer>
+      <Table stickyHeader>
+        <TableHead>
+          <TableRow>
+            <TableCell>CID</TableCell>
+            <TableCell>Type</TableCell>
+            <TableCell>Last Accessed</TableCell>
+            <TableCell></TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {cidAnalytics.map((content) => (
+            <TableRow key={content.cid}>
+              <TableCell>{content.cid}</TableCell>
+              <TableCell>
+                <Chip
+                  aria-label="cid type"
+                  color={getCidTypeChipColor(content.cidType, {})}
+                  label={content.cidType.toUpperCase()}
+                  variant="filled"
+                />
+              </TableCell>
+              <TableCell>{getFormattedDate(content.lastAccessed)}</TableCell>
+              <TableCell>
+                <IconButton onClick={() => onRemove(content)}>
+                  <DeleteOutlined />
+                </IconButton>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
+
+CIDTable.propTypes = {
+  cidAnalytics: PropTypes.object.isRequired,
+  isLoading: PropTypes.bool,
+  onRemove: PropTypes.func.isRequired,
+};
--- src/pages/cids/index.jsx
+++ src/pages/cids/index.jsx
@@ -1,91 +1,13 @@
-/* eslint-disable react/prop-types */
+import { useCallback, useEffect, useState } from "react";
+
+import { Box, Button } from "@mui/material";
 import TextField from "@mui/material/TextField";
-import Typography from "@mui/material/Typography";
+import { SaveOutlined } from "@ant-design/icons";
 
-import { MainCard } from "../../components/molecules/mainCard";
-import {
-  Box,
-  Button,
-  Chip,
-  CircularProgress,
-  IconButton,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
-} from "@mui/material";
-import { DeleteOutlined, SaveOutlined } from "@ant-design/icons";
-import { useState } from "react";
 import { FETCHING_STATES, useFetch } from "../../hooks/useFetch";
-import { useEffect } from "react";
+import { MainCard } from "../../components/molecules/mainCard";
 import { getAnalytics } from "../../api/analytics.api";
-import { getFormattedDate } from "../../libs/utils/date.helpers";
-import { useCallback } from "react";
-
-function getCidTypeChipColor(cidType) {
-  switch (cidType) {
-    case "ipfs":
-      return "success";
-    case "ipns":
-      return "error";
-    default:
-      return "primary";
-  }
-}
-
-/**
- * @param {{ cids: any[], isLoading: boolean, onRemove: function }} props
- * @returns
- */
-function CIDTable({ cidAnalytics = [], isLoading = false, onRemove }) {
-  if (isLoading) {
-    return <CircularProgress variant="indeterminate" />;
-  }
-
-  if (cidAnalytics.length === 0) {
-    return (
-      <Typography variant="body1">No CIDs are being tracked by you</Typography>
-    );
-  }
-
-  return (
-    <TableContainer>
-      <Table stickyHeader>
-        <TableHead>
-          <TableRow>
-            <TableCell>CID</TableCell>
-            <TableCell>Type</TableCell>
-            <TableCell>Last Accessed</TableCell>
-            <TableCell></TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {cidAnalytics.map((content) => (
-            <TableRow key={content.cid}>
-              <TableCell>{content.cid}</TableCell>
-              <TableCell>
-                <Chip
-                  aria-label="cid type"
-                  color={getCidTypeChipColor(content.cidType)}
-                  label={content.cidType.toUpperCase()}
-                  variant="filled"
-                />
-              </TableCell>
-              <TableCell>{getFormattedDate(content.lastAccessed)}</TableCell>
-              <TableCell>
-                <IconButton onClick={() => onRemove(content)}>
-                  <DeleteOutlined />
-                </IconButton>
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
-  );
-}
+import { CIDTable } from "../../components/organisms/cidTable";
 
 function CIDForm() {
   const [cidValue, setCIDValue] = useState("");
@@ -102,6 +24,7 @@ function CIDForm() {
     <Box alignItems="flex-start" display="flex" gap={2} flexDirection="column">
       <TextField
         label="CID"
+        spellCheck={false}
         onKeyUp={handleChange}
         placeholder="Example CID: bafybeiedv7sowwxamly4oicivudp45rsfvbklnf3fvbvonxrwoxqylhtwq"
         fullWidth
