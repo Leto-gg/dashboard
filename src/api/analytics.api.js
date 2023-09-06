58a07722c213cfe2190eb4a1ed717146aa1e7100
Yasir Nadeem <m-yasir.dev@protonmail.com>
2023-09-07 03:28:31 +0500

    feat(analytics-api): add api endpoint
    feat(analytics-api): set api to fetch last  7 days analytics

--- src/api/analytics.api.js
+++ src/api/analytics.api.js
@@ -1,68 +1,20 @@
+import { httpClient } from "../libs/axios";
+import { objToQueryString } from "../libs/utils/object.helpers";
+
+const DEFAULT_RANGE = {
+  from: new Date(new Date().setDate(new Date().getDate() - 7)).toISOString(),
+  to: new Date().toISOString(),
+};
+
 /**
- * @param {string[]} cids
  * @param {RangeQuery} range
  */
-export function getAnalytics(cids, range = {}) {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      console.log({ cids, range });
-      // fetch("https://api.leto.gg/v1/analytics", {
-      //   headers: {
-      //     // add auth header
-      //   }
-      // })
-      // Return mock response temporarily
-      resolve({
-        success: true,
-        data: {
-          metadata: [
-            {
-              total: 2,
-              page: 1,
-            },
-          ],
-          data: [
-            {
-              cid: "bafkreiedazv25tc7idkywbco3jxrywdnb3g4ff3pqegrmshhtcpk5zgsyi",
-              cidType: "ipfs",
-              lastAccessed: "2023-04-10T22:16:42.813Z",
-              numbersAccessed: 33,
-            },
-            {
-              cid: "bafkreiedazv25tc7idkywbco3jxrywdnb3g4ff3pqegrmshhtcpkdzg2zx",
-              cidType: "ipfs",
-              lastAccessed: "2023-04-10T22:16:42.813Z",
-              numbersAccessed: 40,
-            },
-            {
-              cid: "bafkreigt25bt7a6fdnx72jhtnnoezvoihcxxlvyc6kpxkfonayxrqnbf2e",
-              cidType: "ipfs",
-              lastAccessed: "2023-04-07T09:42:14.531Z",
-              numbersAccessed: 13,
-            },
-            {
-              cid: "bafkreigt25bt7a6fdnx72jhtnnoezvoihcxxlvyc6kpxkfonafxrfnv3ce",
-              cidType: "ipns",
-              lastAccessed: "2023-04-07T09:42:14.531Z",
-              numbersAccessed: 23,
-            },
-            {
-              cid: "bafkreigt25bt7a6fdnx72jhtnnoezvoihcxxlvyc6kpxkfonayxrqndiie",
-              cidType: "ipfs",
-              lastAccessed: "2023-04-07T09:42:14.531Z",
-              numbersAccessed: 18,
-            },
-            {
-              cid: "bafkreigt25bt7a6fdnx72jhtnnoezvoihcxxlvyc6kpxkfonayxrqnpiie",
-              cidType: "ipfs",
-              lastAccessed: "2023-04-07T09:42:14.531Z",
-              numbersAccessed: 23,
-            },
-          ],
-        },
-      });
-    }, 500);
-  });
+export function getAnalytics(range = DEFAULT_RANGE) {
+  let apiUrl = "/dashboard";
+  if (Object.keys(range)) {
+    apiUrl = `${apiUrl}?${objToQueryString(range)}`;
+  }
+  return httpClient.get(apiUrl);
 }
 
 /**
