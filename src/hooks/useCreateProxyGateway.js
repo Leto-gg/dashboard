237d7ded696efb971fdbb98a241bcc3be988b2c5
Yasir Nadeem <m-yasir.dev@protonmail.com>
2023-11-01 12:22:11 +0500

    feat: add query hooks for proxy gateway feature

---
+++ src/hooks/useCreateProxyGateway.js
@@ -0,0 +1,22 @@
+import { useMutation, useQueryClient } from "react-query";
+import { httpClient } from "../libs/axios";
+import { QUERY_KEY } from "../libs/constants/query";
+
+function useCreateProxyGateway() {
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation(async ({ proxyName, gatewayURL }) => {
+    const result = await httpClient.post(`/gateways`, {
+      proxyName,
+      gatewayURL,
+    });
+
+    queryClient.setQueryData(QUERY_KEY.PROXY_GATEWAY, result);
+
+    return result;
+  });
+
+  return mutation;
+}
+
+export default useCreateProxyGateway;
