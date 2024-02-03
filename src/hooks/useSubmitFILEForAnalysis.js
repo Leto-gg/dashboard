1e38ecc5f99f04727d8f161d5ffdb9643016f68e
Logan Lentz <logan.lentz1@gmail.com>
2024-02-02 19:47:18 -0600

    send userID like CID upload button

--- src/hooks/useSubmitFILEForAnalysis.js
+++ src/hooks/useSubmitFILEForAnalysis.js
@@ -1,25 +1,16 @@
-import { analyzeFile } from "../api/malware-analyzer.api";
 import { useMutation } from "react-query";
+import { analyzeFile } from "../api/malware-analyzer.api";
 
 export function useSubmitFILEForAnalysis(userId) {
-  return useMutation(
-    (file) => {
-      if (!userId) {
-        // Instead of throwing an error, returning a rejected promise.
-        return Promise.reject(new Error("No user ID provided"));
-      }
-      return analyzeFile(file, userId);
-    },
-    {
-      // Optional: Define onSuccess and onError handlers
-      onSuccess: (data) => {
-        // Handle a successful file submission
-        console.log("File submitted successfully:", data);
-      },
-      onError: (error) => {
-        // Handle any errors in file submission
-        console.error("Error submitting file:", error);
-      },
-    }
-  );
+  const mutation = useMutation((file) => {
+    if (userId)
+      return analyzeFile({
+        file,
+        userId,
+      });
+    else
+      return Promise.reject(new Error("No user ID provided"));
+  });
+
+  return mutation;
 }
